2ab5d959962caf6fcee46a5da07edd056d8ece39
c91yhi <c91yhi@lg.com>
2023-12-12 11:55:44 +0800

    Singup & Login page

--- pubble/core/forms.py
+++ pubble/core/forms.py
@@ -1,9 +1,41 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 
 
+class LoginForm(AuthenticationForm):
+    username = forms.CharField(widget=forms.TextInput(attrs={
+        'placeholder': 'Your username',
+        'class': 'w-full py-4 px-6 rounded-xl'
+    }))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': 'Your password',
+        'class': 'w-full py-4 px-6 rounded-xl'
+    }))
+
+
 class SignupForm(UserCreationForm):
+    # 表單屬性
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
+
+    # 在 username 的位置用上 Css 樣式
+    username = forms.CharField(widget=forms.TextInput(attrs={
+        'placeholder': 'Your username',
+        'class': 'w-full py-4 px-6 rounded-xl'
+    }))
+    # 使用 EmailField 可以驗證 Email 格式
+    email = forms.CharField(widget=forms.EmailInput(attrs={
+        'placeholder': 'Your email',
+        'class': 'w-full py-4 px-6 rounded-xl'
+    }))
+    # 當輸入 password 時可以轉成未識別方式
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': 'Your password',
+        'class': 'w-full py-4 px-6 rounded-xl'
+    }))
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': 'Repeat password',
+        'class': 'w-full py-4 px-6 rounded-xl'
+    }))
